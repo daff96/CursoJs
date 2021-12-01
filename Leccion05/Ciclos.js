e86038b169aaff943d318ebb058cb663109ab31d
daff96 <ddafstrange@gmail.com>
2021-11-30 18:24:43 -0600

    Leccion06

--- Leccion05/Ciclos.js
+++ Leccion05/Ciclos.js
@@ -1,7 +1,21 @@
+/*
 let contador = 0;
-
 while ( contador < 3 ) {
   console.log ( contador );
   contador++;
 }
 console.log ( "Fin ciclo while" );
+
+//do while
+
+do {
+  console.log ( contador );
+  contador ++;
+} while ( contador < 3 );
+console.log ( "Fin ciclo do while" );
+*/
+
+for ( let contador = 0; contador < 3 ; contador ++ ){
+  console.log ( contador );
+}
+console.log ( "Fin ciclo for" );
---
+++ Leccion06/0601Arreglos.js
@@ -0,0 +1,5 @@
+const autos = { "BMW", "Mercedes Benz","Volvo"};
+console.log ( autos );
+
+console.log ( autos [0] );
+console.log ( autos [2] );
